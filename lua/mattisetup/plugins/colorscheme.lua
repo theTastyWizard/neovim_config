eaf6d807d3b70d50a9ec147cd78a1d63fea2093c
Matti <matti@Matti-PC.>
2024-01-11 23:52:21 +0000

    bætti við lualine,dressing og tók fortran úr treesitter

--- lua/mattisetup/plugins/colorscheme.lua
+++ lua/mattisetup/plugins/colorscheme.lua
@@ -17,6 +17,16 @@ return{
 	end,
 }
 
+
+-- return{
+--     "rebelot/kanagawa.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	opts = {},
+-- 	config = function()
+-- 		vim.cmd[[colorscheme kanagawa]]
+-- 	end,
+-- }
 --return{
     --'stevedylandev/flexoki-nvim',
 	--lazy = false,
