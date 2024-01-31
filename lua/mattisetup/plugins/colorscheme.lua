2a05dce565939d1ea6ef0a7b92c0f22649e39aa2
theTastywizard <mattipatti1998@gmail.com>
2024-01-31 14:58:30 +0000

    Lagaði colorscheme dótið, þarf að gera meira samt.

--- lua/mattisetup/plugins/colorscheme.lua
+++ lua/mattisetup/plugins/colorscheme.lua
@@ -1,38 +1,25 @@
---return{
-	--"folke/tokyonight.nvim",
-	--lazy = false,
-	--priority = 1000,
-	--opts = {},
-	--config = function()
-		--vim.cmd[[colorscheme tokyonight-night]]
-	--end,
---}
-return{
-    "Shatur/neovim-ayu",
-	lazy = false,
-	priority = 1000,
-	opts = {},
-	config = function()
-		vim.cmd[[colorscheme ayu-dark]]
-	end,
+return {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+
+    {
+        "EdenEast/nightfox.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+
+    {
+        "Shatur/neovim-ayu",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function()
+            vim.cmd [[colorscheme ayu-dark]]
+        end,
+    },
 }
-
-
--- return{
---     "rebelot/kanagawa.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	opts = {},
--- 	config = function()
--- 		vim.cmd[[colorscheme kanagawa]]
--- 	end,
--- }
---return{
-    --'stevedylandev/flexoki-nvim',
-	--lazy = false,
-	--priority = 1000,
-	--opts = {},
-	--config = function()
-		--vim.cmd[[colorscheme flexoki]]
-	--end,
---}
