3354818d3b422436652fe4a768ab400031504b7e
theTastywizard <mattipatti1998@gmail.com>
2024-01-31 14:57:25 +0000

    breytti gf í sg fyrir search git.

--- lua/mattisetup/plugins/telescope.lua
+++ lua/mattisetup/plugins/telescope.lua
@@ -1,8 +1,9 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-		vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-	end
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        vim.keymap.set('n', '<leader>sg', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+        vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+    end
 }
