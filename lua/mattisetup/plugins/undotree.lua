50af7dcfd71128938cedddbbf5c40ead7a989272
theTastywizard <mattipatti1998@gmail.com>
2024-02-01 19:24:09 +0000

    Fín pússun og reyna bæta við toggle í undotree.

--- lua/mattisetup/plugins/undotree.lua
+++ lua/mattisetup/plugins/undotree.lua
@@ -1,6 +1,9 @@
 return {
     'mbbill/undotree',
     config = function()
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, {desc='Toggle Undotree'})
-    end
+        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = 'Toggle Undotree' })
+        -- virkar ekki en veit ekki afhverju
+        -- vim.g:undotree_SplitWidth = 40
+        -- vim.g:undotree_SetFocusWhenToggle = 1
+    end,
 }
