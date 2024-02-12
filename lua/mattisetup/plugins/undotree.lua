50878485ae7eea8be3fe047501e25e7d5b7e6596
theTastywizard <mattipatti1998@gmail.com>
2024-02-12 14:57:02 +0000

    stækkaði windoið og set focus.

--- lua/mattisetup/plugins/undotree.lua
+++ lua/mattisetup/plugins/undotree.lua
@@ -2,8 +2,7 @@ return {
     'mbbill/undotree',
     config = function()
         vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = 'Toggle Undotree' })
-        -- virkar ekki en veit ekki afhverju
-        -- vim.g:undotree_SplitWidth = 40
-        -- vim.g:undotree_SetFocusWhenToggle = 1
+        vim.g.undotree_SplitWidth = 40
+        vim.g.undotree_SetFocusWhenToggle = 1
     end,
 }
