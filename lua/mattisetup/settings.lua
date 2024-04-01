b2033db2c3ea3609291c9f21cf95ab49d38fc4ba
Matti <matti@Matti-PC.>
2024-04-01 22:55:26 +0000

    bara passa að smarttab sé á, er default á

--- lua/mattisetup/settings.lua
+++ lua/mattisetup/settings.lua
@@ -5,6 +5,7 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.smarttab = true
 
 -- Fyrir undotree plugin svo það muni lengi fra primeagen
 -- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
