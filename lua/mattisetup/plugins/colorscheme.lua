5165030553f175b0edec7e0bae56dadc22c5489e
Matti <matti@Matti-PC.>
2024-04-02 22:40:30 +0000

    Breyta yfir í hitt base16 plugin, fyrir lualine

--- lua/mattisetup/plugins/colorscheme.lua
+++ lua/mattisetup/plugins/colorscheme.lua
@@ -1,22 +1,25 @@
 return {
-    -- {
-    --     "RRethy/base16-nvim",
-    --     lazy = false,
-    --     priority = 1000,
-    --     opts = {},
-    --     config = function ()
-    --         -- vim.cmd[[colorscheme base16-pinky]]
-    --     end
-    -- },
     {
-        "bradcush/nvim-base16",
+        "RRethy/base16-nvim",
         lazy = false,
         priority = 1000,
         opts = {},
-        config = function()
-            vim.cmd [[colorscheme base16-pinky]]
+        config = function ()
+            require('base16-colorscheme').with_config({
+                telescope = false,
+            })
+            vim.cmd[[colorscheme base16-pinky]]
         end
     },
+    -- {
+    --     "bradcush/nvim-base16",
+    --     lazy = false,
+    --     priority = 1000,
+    --     opts = {},
+    --     config = function()
+    --         -- vim.cmd [[colorscheme base16-pinky]]
+    --     end
+    -- },
     {
         "Shatur/neovim-ayu",
         lazy = false,
