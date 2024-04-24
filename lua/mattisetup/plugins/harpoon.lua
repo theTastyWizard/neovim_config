fbdeb972fb3ac928a5e44b44d3beda47988d3172
TheTastyWizard <>
2024-04-24 21:50:16 +0000

    nota j-l fyrir 3 skrár

--- lua/mattisetup/plugins/harpoon.lua
+++ lua/mattisetup/plugins/harpoon.lua
@@ -1,14 +1,13 @@
-return {"ThePrimeagen/harpoon",
-	lazy = false,
-	dependencies = {"nvim-lua/plenary.nvim"},
-	config = true,
-	keys = {
-		{ "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Mark file with harpoon" },
-		{ "<leader>a", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
-		{ "<leader>h", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", desc = "go to file 1"},
-		{ "<leader>j", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", desc = "go to file 2"},
-		{ "<leader>k", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", desc = "go to file 3"},
-		{ "<leader>l", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", desc = "go to file 4"},
-
-	},
+return {
+    "ThePrimeagen/harpoon",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,
+    keys = {
+        { "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<cr>",        desc = "Mark file with harpoon" },
+        { "<leader>a", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+        { "<leader>j", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",         desc = "go to file 1" },
+        { "<leader>k", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",         desc = "go to file 2" },
+        { "<leader>l", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",         desc = "go to file 3" },
+    },
 }
