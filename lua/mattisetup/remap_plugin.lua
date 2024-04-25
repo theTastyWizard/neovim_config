b7d9b010ff6191f9fe701509c2958af9d472ea3f
TheTastyWizard <>
2024-04-25 22:32:51 +0000

    fín pússa

--- lua/mattisetup/remap_plugin.lua
+++ lua/mattisetup/remap_plugin.lua
@@ -1,17 +1,15 @@
 local wk = require("which-key")
 
-vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", {desc='Open Oil'})
+vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = 'Open Oil' })
 
 wk.register({
     ["<leader>d"] = {
         name = "Diagnostics",
-        n = {"<cmd>lua vim.diagnostic.goto_next()<cr>", 'go to next diagnostic'},
-        p = {"<cmd>lua vim.diagnostic.goto_prev()<cr>", 'go to previous diagnostic'},
-        l = {require("lsp_lines").toggle, 'Toggle lsp_lines'},
-        t = {"<cmd>TroubleToggle<cr>", 'open Trouble'},
-        T = {"<cmd>TodoTrouble<cr>", 'open Todo comments in Trouble'},
+        n = { "<cmd>lua vim.diagnostic.goto_next()<cr>", 'go to next diagnostic' },
+        p = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", 'go to previous diagnostic' },
+        l = { require("lsp_lines").toggle, 'Toggle lsp_lines' },
+        t = { "<cmd>TroubleToggle<cr>", 'open Trouble' },
+        T = { "<cmd>TodoTrouble<cr>", 'open Todo comments in Trouble' },
     },
 
 })
-
-
