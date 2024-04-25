1ea6302cb801a536f054242f41467d43b3fcff47
TheTastyWizard <>
2024-04-25 18:20:02 +0000

    Breyta visual highlight og comment í base16 pinky

--- lua/mattisetup/plugins/colorscheme.lua
+++ lua/mattisetup/plugins/colorscheme.lua
@@ -4,11 +4,29 @@ return {
         lazy = false,
         priority = 1000,
         opts = {},
-        config = function ()
+        config = function()
             require('base16-colorscheme').with_config({
                 telescope = false,
             })
-            vim.cmd[[colorscheme base16-pinky]]
+            require('base16-colorscheme').setup({ --óbreytt er pinky
+                base00 = '#171517',
+                base01 = '#1b181b',
+                base02 = '#333133', --breytt til að sjá betur
+                base03 = '#937979', --breytt til að sjá betur TODO: Velja aðeins betri lit
+                base04 = '#e7dbdb',
+                base05 = '#f5f5f5',
+                base06 = '#ffffff',
+                base07 = '#f7f3f7',
+                base08 = '#ffa600',
+                base09 = '#00ff66',
+                base0A = '#20df6c',
+                base0B = '#ff0066',
+                base0C = '#6600ff',
+                base0D = '#00ffff',
+                base0E = '#007fff',
+                base0F = '#df206c'
+            })
+            -- vim.cmd [[colorscheme base16-pinky]]
         end
     },
     -- {
