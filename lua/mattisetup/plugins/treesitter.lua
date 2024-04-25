46addfc6824afba4403f941c629660ff5eca7730
TheTastyWizard <>
2024-04-25 18:29:15 +0000

    hafa fleiri parsera í ensure installed

--- lua/mattisetup/plugins/treesitter.lua
+++ lua/mattisetup/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "fortran", "python", "bash", "fish" ,"java"},
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "fortran", "python", "bash", "fish" ,"java", "markdown", "markdown_inline", "make", "nix", "regex"},
             sync_install = false,
             highlight = { enable = true },
             indent = {
