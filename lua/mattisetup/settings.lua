ce085c6d844a2e7f9d6320a431d8ab0a9fab757c
theTastywizard <mattipatti1998@gmail.com>
2024-06-04 15:29:19 +0000

    bæta við fleirri stillingum

--- lua/mattisetup/settings.lua
+++ lua/mattisetup/settings.lua
@@ -17,12 +17,37 @@ vim.opt.undodir = os.getenv("HOME") .. "/.local/state/nvim/undo"
 vim.opt.undofile = true
 
 -- https://www.reddit.com/r/vim/comments/jqogan/how_does_a_lower_updatetime_lead_to_better/
-vim.opt.updatetime = 50
+vim.opt.updatetime = 2000
 
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 999
 
+vim.opt.cursorline = true
+
+vim.opt.splitkeep = 'screen'
+
+vim.opt.list = true
+vim.opt.listchars = {
+    -- Replace tab whitespace with ->
+    tab = '->',
+    -- I don't care about spaces so spaces are just plain old whitespace
+    multispace = ' ',
+    -- nbsp=' ',
+    -- If a line ends with a space, I probably do care about that, show this instead of whitespace
+    -- trail = '',
+    -- If the line goes off the screen (for some reason), show this as the last character of the line so I know the
+    -- line continues to the right
+    extends = '⟩',
+    -- Literally the same but left
+    precedes = '⟨'
+}
+-- Tells me if there is the line is wrapped
+vim.g.showbreak = '↪' --NOTE: virðist ekki virka
+
 -- TODO: virkar ekki þarf að setja autacmd
 -- á að taka burt nýtt line með comment á o
-vim.opt.formatoptions:remove("o")
+-- vim.opt.formatoptions:remove("o")
+
+
+
