72c69ddae9d6419994fe97e8972208390a598d01
theTastywizard <mattipatti1998@gmail.com>
2024-06-24 14:35:17 +0000

    breyta í leader h

--- lua/mattisetup/plugins/harpoon.lua
+++ lua/mattisetup/plugins/harpoon.lua
@@ -5,7 +5,7 @@ return {
     config = true,
     keys = {
         { "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<cr>",        desc = "Mark file with harpoon" },
-        { "<leader>a", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+        { "<leader>h", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
         { "<leader>j", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",         desc = "Harpoon file 1" },
         { "<leader>k", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",         desc = "Harpoon file 2" },
         { "<leader>l", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",         desc = "Harpoon file 3" },
