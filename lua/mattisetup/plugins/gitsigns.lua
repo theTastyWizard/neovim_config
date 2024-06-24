10da20e9d928996122e906fcec849baa5f6787b6
theTastywizard <mattipatti1998@gmail.com>
2024-06-24 14:34:50 +0000

    setja rounded borders

--- lua/mattisetup/plugins/gitsigns.lua
+++ lua/mattisetup/plugins/gitsigns.lua
@@ -1,6 +1,9 @@
 return {
     "lewis6991/gitsigns.nvim",
     config = function()
-        require('gitsigns').setup()
+        require('gitsigns').setup({
+            preview_config = {
+            border = 'rounded'}
+        })
     end,
 }
