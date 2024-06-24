660589357fb4c3b65a27cf2e463038848cf3145c
theTastywizard <mattipatti1998@gmail.com>
2024-06-24 14:41:19 +0000

    plugin til að færa sig með camelCase og fleira

---
+++ lua/mattisetup/plugins/spider.lua
@@ -0,0 +1,31 @@
+return {
+    "chrisgrieser/nvim-spider",
+    lazy = false,
+    config = function()
+        require("spider").setup({
+            skipInsignificantPunctuation = true,
+            consistentOperatorPending = true, -- see "Consistent Operator-pending Mode" in the README
+            subwordMovement = true,
+            customPatterns = {},              -- check "Custom Movement Patterns" in the README for details
+        }
+        )
+        vim.keymap.set(
+            { "n", "o", "x" },
+            "w",
+            "<cmd>lua require('spider').motion('w')<CR>",
+            { desc = "Spider-w" }
+        )
+        vim.keymap.set(
+            { "n", "o", "x" },
+            "e",
+            "<cmd>lua require('spider').motion('e')<CR>",
+            { desc = "Spider-e" }
+        )
+        vim.keymap.set(
+            { "n", "o", "x" },
+            "b",
+            "<cmd>lua require('spider').motion('b')<CR>",
+            { desc = "Spider-b" }
+        )
+    end
+}
