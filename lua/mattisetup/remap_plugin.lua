7e9fca4107080ce0b8e5dd645406d899b0cee1bd
theTastywizard <mattipatti1998@gmail.com>
2024-06-24 14:39:36 +0000

    setja upp floatterm mapping

--- lua/mattisetup/remap_plugin.lua
+++ lua/mattisetup/remap_plugin.lua
@@ -3,6 +3,14 @@ local wk = require("which-key")
 vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = 'Open Oil' })
 
 wk.register({
+    ["<leader>t"] = {
+        name = "Terminal",
+        t = {"<cmd>FloatermNew --name=Main --wintype=split --height=0.25 fish <cr>",
+            'Open Main Terminal'},
+        c = {"<cmd>:FloatermNew --autoclose=0 gcc % -o %< && ./%<<cr>", 'Run C code'},
+    }
+})
+wk.register({
     ["<leader>d"] = {
         name = "Diagnostics",
         n = { "<cmd>lua vim.diagnostic.goto_next()<cr>", 'go to next diagnostic' },
