ea754ec3f08fe69590ed42520b173b334d22f2ee
theTastywizard <mattipatti1998@gmail.com>
2024-07-14 11:43:08 +0000

    bæta við fleiri treesitter

--- lua/mattisetup/plugins/treesitter.lua
+++ lua/mattisetup/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "fortran", "python", "bash", "fish" ,"java", "markdown", "markdown_inline", "make", "nix", "regex"},
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "fortran", "python", "bash", "fish" ,"java", "markdown", "markdown_inline", "make", "nix", "regex", "rust", "cpp"},
             sync_install = false,
             highlight = { enable = true },
             indent = {
