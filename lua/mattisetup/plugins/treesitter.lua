b04b3a7352b2be27e4e0a2afd69fbf59b78b61c0
theTastywizard <mattipatti1998@gmail.com>
2024-07-16 16:06:29 +0000

    prufa að nota indent með python eftir update

--- lua/mattisetup/plugins/treesitter.lua
+++ lua/mattisetup/plugins/treesitter.lua
@@ -5,12 +5,12 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "fortran", "python", "bash", "fish" ,"java", "markdown", "markdown_inline", "make", "nix", "regex", "rust", "cpp"},
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "fortran", "python", "bash", "fish", "java", "markdown", "markdown_inline", "make", "nix", "regex", "rust", "cpp" },
             sync_install = false,
             highlight = { enable = true },
             indent = {
                 enable = true,
-                disable = { "python", "fortran" }
+                disable = { "fortran" }
             },
 
         })
