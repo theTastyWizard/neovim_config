cec5cda30dffdfbfe4e1f936388ce51515ed1bab
theTastywizard <mattipatti1998@gmail.com>
2024-09-02 12:08:35 +0000

    bæta við restina af cmp-cmdline

--- lua/mattisetup/plugins/lsp.lua
+++ lua/mattisetup/plugins/lsp.lua
@@ -67,8 +67,21 @@ return {
             cmp.setup.cmdline('/', {
                 mapping = cmp.mapping.preset.cmdline(),
                 sources = {
-                    { name = 'buffer'}
+                    { name = 'buffer' }
                 }
+            })
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    {
+                        name = 'cmdline',
+                        option = {
+                            ignore_cmds = { 'Man', '!' }
+                        }
+                    }
+                })
             })
         end
     },
