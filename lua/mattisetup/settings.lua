42b9675c415c382df0b46fbfe122694b3c53bc5c
theTastywizard <mattipatti1998@gmail.com>
2024-09-02 12:09:03 +0000

    commenta út windows stillingar

--- lua/mattisetup/settings.lua
+++ lua/mattisetup/settings.lua
@@ -84,18 +84,18 @@ vim.opt.listchars = {
     nbsp = "␣",
 }
 
-vim.g.clipboard = {
-    name = "win32yank-wsl",
-    copy = {
-        ["+"] = "win32yank.exe -i --crlf",
-        ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-        ["+"] = "win32yank.exe -o --lf",
-        ["*"] = "win32yank.exe -o --lf",
-    },
-    cache_enabled = true,
-}
+-- vim.g.clipboard = {
+--     name = "win32yank-wsl",
+--     copy = {
+--         ["+"] = "win32yank.exe -i --crlf",
+--         ["*"] = "win32yank.exe -i --crlf",
+--     },
+--     paste = {
+--         ["+"] = "win32yank.exe -o --lf",
+--         ["*"] = "win32yank.exe -o --lf",
+--     },
+--     cache_enabled = true,
+-- }
 
 -- Tells me if there is the line is wrapped
 -- vim.g.showbreak = '↪' --NOTE: virðist ekki virka
