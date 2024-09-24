1d8a2d9fb17e8fb1a9f9ffefc0aec3d845b54d8e
theTastyWizard <mattipatti1998@gmail.com>
2024-09-24 09:22:54 +0000

    vimtex og conjure

---
+++ lua/mattisetup/plugins/conjure.lua
@@ -0,0 +1,15 @@
+return {
+    "Olical/conjure",
+    ft = { "clojure", "fennel", "python", "racket", "scheme" }, -- etc
+    lazy = true,
+    init = function()
+        -- Set configuration options here
+        -- Uncomment this to get verbose logging to help diagnose internal Conjure issues
+        -- This is VERY helpful when reporting an issue with the project
+        -- vim.g["conjure#debug"] = true
+        vim.g["conjure#log#hud#border"] = "rounded"
+        vim.g["vim.g:conjure#log#hud#ignore_low_priority"] = true
+        vim.g["conjure#mapping#doc_word"] = false
+        vim.g["listp_rainbow"] = true
+    end,
+}
