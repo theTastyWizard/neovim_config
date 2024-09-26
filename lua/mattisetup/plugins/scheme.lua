da55cf99b62a1c69ede3ce42d02832b2729d9c08
theTastyWizard <mattipatti1998@gmail.com>
2024-09-26 17:00:32 +0000

    setja að repl byrji ekki sjálfkrafa (dóu aldrei á windows)

--- lua/mattisetup/plugins/scheme.lua
+++ lua/mattisetup/plugins/scheme.lua
@@ -8,6 +8,7 @@ return {
             -- Uncomment this to get verbose logging to help diagnose internal Conjure issues
             -- This is VERY helpful when reporting an issue with the project
             -- vim.g["conjure#debug"] = true
+            vim.g["conjure#client_on_load"] = false
             vim.g["conjure#log#hud#border"] = "rounded"
             vim.g["vim.g:conjure#log#hud#ignore_low_priority"] = true
             vim.g["conjure#mapping#doc_word"] = false
