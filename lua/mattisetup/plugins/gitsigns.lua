e521bde18b88303a2c34f79185341951919d66f7
theTastyWizard <mattipatti1998@gmail.com>
2024-10-03 23:24:44 +0000

    bæta við valmöguleikum á gitsigns

--- lua/mattisetup/plugins/gitsigns.lua
+++ lua/mattisetup/plugins/gitsigns.lua
@@ -2,9 +2,30 @@ return {
     "lewis6991/gitsigns.nvim",
     config = function()
         require('gitsigns').setup({
+            signs = {
+                add          = { text = '┃', count = true },
+                change       = { text = '┃', count = true },
+                delete       = { text = '_', count = true },
+                topdelete    = { text = '‾', count = true },
+                changedelete = { text = '~', count = true },
+                untracked    = { text = '┆' },
+            },
             numhl = true,
             preview_config = {
-            border = 'rounded'}
+                border = 'rounded' },
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+                delay = 0,
+                ignore_whitespace = false,
+                virt_text_priority = 100,
+                use_focus = true,
+            },
+            current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+            diff_opts = {
+                vertical = false,
+            },
+            trouble = true,
         })
     end,
 }
