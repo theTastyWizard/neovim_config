554d40abd4bc9175bb51922628ea83860083c178
theTastywizard <mattipatti1998@gmail.com>
2024-10-07 20:41:47 +0000

    setja inn helling í auto install -kannski mistök :o

--- lua/mattisetup/plugins/treesitter.lua
+++ lua/mattisetup/plugins/treesitter.lua
@@ -1,18 +1,18 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    config = function()
-        local configs = require("nvim-treesitter.configs")
+	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate',
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "fortran", "python", "bash", "fish", "java", "markdown", "markdown_inline", "make", "nix", "regex", "rust", "cpp" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = {
-                enable = true,
-                disable = { "fortran" }
-            },
+		configs.setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "fortran", "python", "bash", "fish", "java", "markdown", "markdown_inline", "make", "nix", "regex", "rust", "cpp", "latex", "asm", "disassembly", "objdump", "html", "css", "json", "ocaml", "ocaml_interface", "sql" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = {
+				enable = true,
+				disable = { "fortran" }
+			},
 
-        })
-    end
+		})
+	end
 }
