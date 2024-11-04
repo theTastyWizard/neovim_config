3b8686b7157895a5811e8a375c77899d01a5e80b
theTastyWizard <mattipatti1998@gmail.com>
2024-11-04 21:27:30 +0000

    taka út ónotað treesitter dót

--- lua/mattisetup/plugins/treesitter.lua
+++ lua/mattisetup/plugins/treesitter.lua
@@ -1,18 +1,21 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "fortran", "python", "bash", "fish", "java", "markdown", "markdown_inline", "make", "nix", "regex", "rust", "cpp", "latex", "asm", "disassembly", "objdump", "html", "css", "json", "ocaml", "ocaml_interface", "sql" },
-			sync_install = false,
-			highlight = { enable = true },
-			indent = {
-				enable = true,
-				disable = { "fortran" }
-			},
+        configs.setup({
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "fortran",
+                "python", "bash", "fish", "markdown", "markdown_inline", "latex",
+                "make", "nix", "regex", "cpp", "asm", "disassembly", "objdump",
+                "html", "css", "json", "ocaml", "ocaml_interface", "sql" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = {
+                enable = true,
+                disable = { "fortran" }
+            },
 
-		})
-	end
+        })
+    end
 }
