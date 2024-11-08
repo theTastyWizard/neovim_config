a9eb62e53b089a64167c4c655c41f436a668afbe
theTastyWizard <mattipatti1998@gmail.com>
2024-11-08 00:44:42 +0000

    láta vimtex henda drasli þegar hætti í buffer

--- lua/mattisetup/autocmds.lua
+++ lua/mattisetup/autocmds.lua
@@ -16,15 +16,25 @@ autocmd("FileType", {
     },
     callback = function(event)
         vim.bo[event.buf].buflisted = false
-        vim.keymap.set("n", "q", "<cmd>close<cr>", {buffer = event.buf, silent = true})
+        vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
     end,
     desc = "Close certain windows with q",
 })
 
--- Ekki commenta á nýar línur 
+-- Ekki commenta á nýar línur
 autocmd("BufEnter", {
-    callback = function ()
-        vim.opt.formatoptions:remove({"c", "r", "o"})
+    callback = function()
+        vim.opt.formatoptions:remove({ "c", "r", "o" })
     end,
     desc = "Disable new line comment",
+})
+
+-- fyrir vimtex
+-- local au_group = vim.api.nvim_create_augroup("vimtex_events", {}),
+
+-- Cleanup on quit
+autocmd("User", {
+    pattern = "VimtexEventQuit",
+    command = "VimtexClean",
+    desc = "Clean folder on vimtex exit"
 })
