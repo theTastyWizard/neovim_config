b568bba5e82840cf15fed18cf0f7257d1d01009b
theTastyWizard <mattipatti1998@gmail.com>
2024-11-19 09:15:35 +0000

    stilla útlitið betur á markdown

--- lua/mattisetup/plugins/markview.lua
+++ lua/mattisetup/plugins/markview.lua
@@ -1,10 +1,38 @@
 return {
 	"OXY2DEV/markview.nvim",
-	lazy = true, -- false Recommended
+	lazy = true,  -- false Recommended
 	ft = "markdown", -- If you decide to lazy-load anyway
 
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"nvim-tree/nvim-web-devicons"
-	}
+	},
+	config = function()
+		local presets = require("markview.presets");
+
+		require("markview").setup({
+			checkboxes = presets.checkboxes.nerd,
+			headings = presets.headings.marker
+		});
+		require("markview.extras.checkboxes").setup({
+			--- When true, list item markers will
+			--- be removed.
+			remove_markers = false,
+
+			--- If false, running the command on
+			--- visual mode doesn't change the
+			--- mode.
+			exit = false,
+
+			default_marker = "-",
+			default_state = "X",
+
+			--- A list of states
+			states = {
+				{ " ", "X" },
+				{ "-", "o", "~" }
+			}
+		});
+	end
+
 }
