58dfc3636b201e42a4f12c634d9a94a2140dda91
theTastyWizard <mattipatti1998@gmail.com>
2024-12-29 16:31:39 +0000

    add monkai-pro colorscheme

--- lua/mattisetup/plugins/colorscheme.lua
+++ lua/mattisetup/plugins/colorscheme.lua
@@ -4,6 +4,63 @@ return {
 		lazy = false,
 	},
 	{
+		"loctvl842/monokai-pro.nvim",
+		config = function()
+			require("monokai-pro").setup({
+				transparent_background = false,
+				terminal_colors = true,
+				devicons = true, -- highlight the icons of `nvim-web-devicons`
+				styles = {
+					comment = { italic = true },
+					keyword = { italic = true }, -- any other keyword
+					type = { italic = true }, -- (preferred) int, long, char, etc
+					storageclass = { italic = true }, -- static, register, volatile, etc
+					structure = { italic = true }, -- struct, union, enum, etc
+					parameter = { italic = true }, -- parameter pass in function
+					annotation = { italic = true },
+					tag_attribute = { italic = true }, -- attribute of tag in reactjs
+				},
+				filter = "spectrum",    -- classic | octagon | pro | machine | ristretto | spectrum
+				-- Enable this will disable filter option
+				day_night = {
+					enable = false, -- turn off by default
+					day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
+					night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
+				},
+				inc_search = "background", -- underline | background
+				background_clear = {
+					-- "float_win",
+					"toggleterm",
+					"telescope",
+					"which-key",
+					"renamer",
+					"notify",
+					-- "nvim-tree",
+					-- "neo-tree",
+					-- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+				}, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+				plugins = {
+					bufferline = {
+						underline_selected = false,
+						underline_visible = false,
+					},
+					indent_blankline = {
+						context_highlight = "default", -- default | pro
+						context_start_underline = false,
+					},
+				},
+				-- ---@param c Colorscheme
+				-- override = function(c) end,
+				-- ---@param cs Colorscheme
+				-- ---@param p ColorschemeOptions
+				-- ---@param Config MonokaiProOptions
+				-- ---@param hp Helper
+				-- override = function(cs: Colorscheme, p: ColorschemeOptions, Config: MonokaiProOptions, hp: Helper) end,
+			})
+			-- vim.cmd [[colorscheme monokai-pro]]
+		end
+	},
+	{
 		"cpwrs/americano.nvim",
 		lazy = false,
 		priority = 1000,
@@ -67,10 +124,10 @@ return {
 				-- base00 = '#111111', --bakgrunnur frá colors
 				-- base00 = '#000000', -- alveg svartur bakgrunnur
 				base01 = '#1b181b',
-				base02 = '#2d2c2d',--visual selected breytt til að sjá betur
-				-- base02 = '#1E3232', 
-				base03 = '#117c3c', --comments breytt til að sjá betur 
-				-- '#169c4b', 
+				base02 = '#2d2c2d', --visual selected breytt til að sjá betur
+				-- base02 = '#1E3232',
+				base03 = '#117c3c', --comments breytt til að sjá betur
+				-- '#169c4b',
 				-- #5100cc'
 				-- #00ff66',
 
@@ -100,17 +157,17 @@ return {
 				base0E = '#007fff',
 				base0F = '#df206c'
 			})
-            vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#120f12' })
-            vim.api.nvim_set_hl(0, 'NormalMode', {fg = "#ffa600", bg = '#120f12'  })
-            vim.api.nvim_set_hl(0, 'InsertMode', {fg = "#ff0066" })
-            vim.api.nvim_set_hl(0, 'VisualMode', {fg = "#00ff66" })
-            vim.api.nvim_set_hl(0, 'CommandMode', {fg = "#6600ff" })
-            vim.api.nvim_set_hl(0, 'ReplaceMode', {fg = "#ffa600" })
-            vim.api.nvim_set_hl(0, 'SelectMode', {fg = "#ffa600" })
-            vim.api.nvim_set_hl(0, 'TerminalMode', {fg = "#ffa600" })
-            vim.api.nvim_set_hl(0, 'TerminalNormalMode', {fg = "#ffa600" })
-            -- vim.cmd [[colorscheme base16-pinky]]
-        end
+			vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#120f12' })
+			vim.api.nvim_set_hl(0, 'NormalMode', { fg = "#ffa600", bg = '#120f12' })
+			vim.api.nvim_set_hl(0, 'InsertMode', { fg = "#ff0066" })
+			vim.api.nvim_set_hl(0, 'VisualMode', { fg = "#00ff66" })
+			vim.api.nvim_set_hl(0, 'CommandMode', { fg = "#6600ff" })
+			vim.api.nvim_set_hl(0, 'ReplaceMode', { fg = "#ffa600" })
+			vim.api.nvim_set_hl(0, 'SelectMode', { fg = "#ffa600" })
+			vim.api.nvim_set_hl(0, 'TerminalMode', { fg = "#ffa600" })
+			vim.api.nvim_set_hl(0, 'TerminalNormalMode', { fg = "#ffa600" })
+			-- vim.cmd [[colorscheme base16-pinky]]
+		end
 	},
 	-- {
 	--     "RRethy/base16-nvim",
