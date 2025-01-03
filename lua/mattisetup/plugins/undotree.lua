4ba38c158485e6ae457dd601cf07fcd7bbf91f31
TheTastyWizard <mattipatti1998@gmail.com>
2025-01-03 22:20:07 +0000

    stækka diffwin

--- lua/mattisetup/plugins/undotree.lua
+++ lua/mattisetup/plugins/undotree.lua
@@ -4,5 +4,6 @@ return {
         vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = 'Toggle Undotree' })
         vim.g.undotree_SplitWidth = 40
         vim.g.undotree_SetFocusWhenToggle = 1
+		vim.g.undotree_WindowLayout = 2
     end,
 }
