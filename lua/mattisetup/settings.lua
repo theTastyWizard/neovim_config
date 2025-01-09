a461372ac5abbf30f3d1700795a023b5e2b50bef
TheTastyWizard <mattipatti1998@gmail.com>
2025-01-09 19:13:04 +0000

    skipta um diagnostic signs

--- lua/mattisetup/settings.lua
+++ lua/mattisetup/settings.lua
@@ -51,10 +51,10 @@ vim.opt.signcolumn = "yes:1"
 vim.diagnostic.config({
     signs = {
         text = {
-            [vim.diagnostic.severity.ERROR] = '󰚌 ', --󰅚
-            [vim.diagnostic.severity.WARN] = ' ', --󰀪
-            [vim.diagnostic.severity.HINT] = '󱠂 ', --󰌶
-            [vim.diagnostic.severity.INFO] = ' ', --
+            [vim.diagnostic.severity.ERROR] = '󰯈', --'󰚌', --󰅚 󰯈
+            [vim.diagnostic.severity.WARN] = '󰀪', --' ', --󰀪
+            [vim.diagnostic.severity.HINT] = '',--'󱠂 ', --󰌶
+            [vim.diagnostic.severity.INFO] ='', --' ',
         },
         -- linehl = {
         -- },
