880652f99b6f3f9a3b0268d410d7eaa31f71e980
TheTastyWizard <mattipatti1998@gmail.com>
2025-01-26 14:34:55 +0000

    treesitter: bæta við bigfiledóti,taka út ónotuð mál og formata

--- lua/mattisetup/plugins/treesitter.lua
+++ lua/mattisetup/plugins/treesitter.lua
@@ -5,17 +5,27 @@ return {
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "fortran",
-				"python", "bash", "fish", "markdown", "markdown_inline", "latex",
-				"make", "nix", "regex", "cpp", "asm", "disassembly", "objdump",
-				"html", "css", "json", "ocaml", "ocaml_interface", "sql" },
+			ensure_installed = {
+				"lua", "vim", "vimdoc", "query",
+				"c", "cpp", "fortran", "sql", "python",
+				"bash", "fish", "make",
+				"markdown", "markdown_inline", "latex",
+				"json", "regex", "html", "css",
+			},
 			sync_install = false,
 			highlight = { enable = true },
 			indent = {
 				enable = true,
 				disable = { "fortran", "cpp" }
 			},
-
+			-- disable fyrir of stór file
+			disable = function(lang, buf)
+				local max_filesize = 100 * 1024 -- 100 KB
+				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+				if ok and stats and stats.size > max_filesize then
+					return true
+				end
+			end,
 		})
 	end
 }
