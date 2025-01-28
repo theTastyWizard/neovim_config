18857ad57f6727caf88b7f7b99f1d986144168a8
theTastyWizard <mattipatti1998@gmail.com>
2025-01-28 20:13:18 +0000

    færa allt litadót í spes skjal

---
+++ lua/mattisetup/plugins/litadot.lua
@@ -0,0 +1,50 @@
+return {
+	{
+		"rktjmp/lush.nvim",
+		cmd = { "Lushify", "LushImport", "LushRunTutorial" },
+		-- if you wish to use your own colorscheme:
+		-- { dir = '/absolute/path/to/colorscheme', lazy = true },
+	},
+	{
+		"catgoose/nvim-colorizer.lua",
+		cond = false,
+		event = "BufReadPre",
+		opts = { -- set to setup table
+			buftypes = {
+				"!snacks_notif"
+			},
+			user_default_options = {
+				names = false,
+				hsl_fn = true,
+				mode = "virtualtext",
+				tailwind = "lsp",
+				virtualtext_inline = false,
+			},
+		},
+	},
+	{
+		'brenoprata10/nvim-highlight-colors',
+		config = function()
+			require('nvim-highlight-colors').setup({
+				enable_named_colors = false,
+				enable_short_hex = false,
+				excluded_filetypes = { "snacks_notif" },
+				excluded_buftypes = { "snacks_notif" }
+			})
+		end
+	},
+	{
+		"uga-rosa/ccc.nvim",
+		cmd = { "CccHighlighterEnable" },
+		config = function()
+			require("ccc").setup({})
+		end
+	},
+	{
+		{ "nvzone/volt", lazy = true },
+		{
+			"nvzone/minty",
+			cmd = { "Shades", "Huefy" },
+		}
+	}
+}
--- lua/mattisetup/plugins/lush.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"rktjmp/lush.nvim",
-	cmd = {"Lushify",  "LushImport", "LushRunTutorial"},
-	-- if you wish to use your own colorscheme:
-	-- { dir = '/absolute/path/to/colorscheme', lazy = true },
-}
