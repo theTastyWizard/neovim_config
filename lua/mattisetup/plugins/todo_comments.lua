2bc5e3e6110f77eaf0ecacd1633bf08eeb006db0
TheTastyWizard <mattipatti1998@gmail.com>
2025-02-09 19:45:33 +0000

    fixup! Todo-comments: loksins að laga config, lowercase etc

--- lua/mattisetup/plugins/todo_comments.lua
+++ lua/mattisetup/plugins/todo_comments.lua
@@ -2,8 +2,8 @@ return {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	opts = {
-		signs = true, -- show icons in the signs column
-		sign_priority = 8, -- sign priority
+		signs = true,          -- show icons in the signs column
+		sign_priority = 8,     -- sign priority
 		-- keywords recognized as todo comments
 		keywords = {
 			FIX = {
@@ -16,31 +16,29 @@ return {
 			HACK = { icon = " ", color = "warning", alt = { "hack" } },
 			WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX", "warn" } },
 			PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE", "perf" } },
-			NOTE = { icon = "  ", color = "hint", alt = { "INFO", "info", "note" } },
+			NOTE = { icon = " ", color = "hint", alt = { "INFO", "info", "note" } },
 			TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED", "test" } },
 		},
 		gui_style = {
-			fg = "NONE", -- The gui style to use for the fg highlight group.
-			bg = "BOLD", -- The gui style to use for the bg highlight group.
+			fg = "NONE",           -- The gui style to use for the fg highlight group.
+			bg = "BOLD",           -- The gui style to use for the bg highlight group.
 		},
-		merge_keywords = true, -- when true, custom keywords will be merged with the defaults
+		merge_keywords = true,     -- when true, custom keywords will be merged with the defaults
 		-- highlighting of the line containing the todo comment
 		-- * before: highlights before the keyword (typically comment characters)
 		-- * keyword: highlights of the keyword
 		-- * after: highlights after the keyword (todo text)
-		-- TODO:
-		-- note:
 		highlight = {
-			multiline = true,       -- enable multine todo comments
-			multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
-			multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
-			before = "",            -- "fg" or "bg" or empty
-			keyword = "bg",         -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
-			after = "fg",           -- "fg" or "bg" or empty
-			pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
-			comments_only = true,   -- uses treesitter to match keywords in comments only
-			max_line_len = 300,     -- ignore lines longer than this
-			exclude = {},           -- list of file types to exclude highlighting
+			multiline = true,                    -- enable multine todo comments
+			multiline_pattern = "^.",            -- lua pattern to match the next multiline from the start of the matched keyword
+			multiline_context = 10,              -- extra lines that will be re-evaluated when changing a line
+			before = "",                         -- "fg" or "bg" or empty
+			keyword = "bg",                    -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+			after = "fg",                        -- "fg" or "bg" or empty
+			pattern = [[.*<(KEYWORDS)\s*:]],     -- pattern or table of patterns, used for highlighting (vim regex)
+			comments_only = true,                -- uses treesitter to match keywords in comments only
+			max_line_len = 300,                  -- ignore lines longer than this
+			exclude = {},                        -- list of file types to exclude highlighting
 		},
 		-- list of named colors where we try to extract the guifg from the
 		-- list of highlight groups or use the hex color if hl not found as a fallback
@@ -63,7 +61,7 @@ return {
 			},
 			-- regex that will be used to match keywords.
 			-- don't replace the (KEYWORDS) placeholder
-			pattern = [[\b(KEYWORDS):]], -- ripgrep regex
+			pattern = [[\b(KEYWORDS):]],     -- ripgrep regex
 			-- pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
 		},
 	}
