4534e1ba82156d9d5efbaf33767a67955443311d
TheTastyWizard <mattipatti1998@gmail.com>
2025-04-24 21:27:06 +0000

    whichkey: setja local buffer keybind og hreinsa

--- lua/mattisetup/plugins/which_key.lua
+++ lua/mattisetup/plugins/which_key.lua
@@ -1,16 +1,26 @@
 return {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    opts = {
-        preset = "modern"
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-        -- win = {
-        --     border = "rounded"
-        -- },
-        -- layout = {
-        --     align = "center",
-        -- }
-    }
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		preset = "modern",
+		-- delay = 100,
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		-- win = {
+		--     border = "rounded"
+		-- },
+		-- layout = {
+		--     align = "center",
+		-- }
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		}
+	},
 }
