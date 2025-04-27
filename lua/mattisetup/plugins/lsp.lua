ca65c7564771d917c7d042e7114c25af9590976e
TheTastyWizard <mattipatti1998@gmail.com>
2025-04-27 16:03:09 +0000

    lsp: breyta keybind fyrir hover a elora

--- lua/mattisetup/plugins/lsp.lua
+++ lua/mattisetup/plugins/lsp.lua
@@ -165,8 +165,8 @@ return {
 				local opts = { buffer = bufnr }
 
 				vim.keymap.set('n', 'æ', '<cmd>lua vim.lsp.buf.hover({border = "rounded"})<cr>', opts)
+				vim.keymap.set('n', ',', '<cmd>lua vim.lsp.buf.hover({border = "rounded"})<cr>', opts)
 				vim.keymap.set('n', 'Æ', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-				vim.keymap.set('n', ';', '<cmd>lua vim.lsp.buf.hover({border = "rounded"})<cr>', opts)
 				vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
 				vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
 				vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
