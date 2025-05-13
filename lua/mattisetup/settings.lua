8a8fe510be91407065e0842d90f6759d884de35c
theTastyWizard <mattipatti1998@gmail.com>
2025-05-13 14:43:22 +0000

    settings: breyta diagnostic icons í minni

--- lua/mattisetup/settings.lua
+++ lua/mattisetup/settings.lua
@@ -55,9 +55,9 @@ vim.diagnostic.config({
     signs = {
         text = {
             [vim.diagnostic.severity.ERROR] = '󰯈', --'󰚌', --󰅚 󰯈
-            [vim.diagnostic.severity.WARN] = '', --'󰀪', --' ', --󰀪
-            [vim.diagnostic.severity.HINT] = '',--'󱠂 ', --󰌶
-            [vim.diagnostic.severity.INFO] ='', --' ',
+            [vim.diagnostic.severity.WARN] = "", --'󰀪', --' ', --󰀪 --
+            [vim.diagnostic.severity.HINT] = '󰌶',--'󱠂 ', --  󰌶 
+            [vim.diagnostic.severity.INFO] = '', --' ',
         },
         -- linehl = {
         -- },
