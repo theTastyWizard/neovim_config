3234617e88ce056cbbe8f474d65186907ac763f3
TheTastyWizard <mattipatti1998@gmail.com>
2025-06-02 18:56:32 +0000

    conform: typst formatter

--- lua/mattisetup/plugins/conform.lua
+++ lua/mattisetup/plugins/conform.lua
@@ -1,38 +1,39 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-        {
-            -- Customize or remove this keymap to your liking
-            "<leader>i",
-            function()
-                require("conform").format({ async = true, lsp_format = "fallback" })
-            end,
-            mode = "",
-            desc = "Format buffer with conform",
-        },
-    },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>i",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = "",
+			desc = "Format buffer with conform",
+		},
+	},
 
-    -- Everything in opts will be passed to setup()
-    opts = {
-        -- Define your formatters
-        formatters_by_ft = {
-            fortran = {"findent"},
-            -- lua = { "stylua" },
-            -- python = { "isort", "black" },
-            -- javascript = { { "prettierd", "prettier" } },
-        },
-        -- Set up format-on-save
-        -- format_on_save = { timeout_ms = 500, lsp_fallback = true },
-        -- Customize formatters
-        formatters = {
-            -- shfmt = {
-            --     prepend_args = { "-i", "2" },
-            -- },
-            findent = {
-                prepend_args = {"-i4"}
-            },
-        },
-    }
+	-- Everything in opts will be passed to setup()
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			fortran = { "findent" },
+			typst = { "typstyle" },
+			-- lua = { "stylua" },
+			-- python = { "isort", "black" },
+			-- javascript = { { "prettierd", "prettier" } },
+		},
+		-- Set up format-on-save
+		-- format_on_save = { timeout_ms = 500, lsp_fallback = true },
+		-- Customize formatters
+		formatters = {
+			-- shfmt = {
+			--     prepend_args = { "-i", "2" },
+			-- },
+			findent = {
+				prepend_args = { "-i4" }
+			},
+		},
+	}
 }
